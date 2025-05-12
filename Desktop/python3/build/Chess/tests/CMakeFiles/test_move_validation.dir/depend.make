42358b7ab023d477dfb574a13a1eb5ddf42367c7
Meari Ibrahim <143554660+ibrahimea@users.noreply.github.com>
2025-05-12 08:12:29 -0700

    Delete Desktop/python3 directory

--- Desktop/python3/build/Chess/tests/CMakeFiles/test_move_validation.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_move_validation.
-# This may be replaced when dependencies are built.
--- Desktop/python3/build/Chess/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ebraheem/CLionProjects/chess-ex1-ibrahimea/Chess/tests
-# Build directory: /home/ebraheem/CLionProjects/chess-ex1-ibrahimea/build/Chess/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ValidateMoves "/home/ebraheem/CLionProjects/chess-ex1-ibrahimea/build/Chess/tests/test_move_validation")
-set_tests_properties(ValidateMoves PROPERTIES  _BACKTRACE_TRIPLES "/home/ebraheem/CLionProjects/chess-ex1-ibrahimea/Chess/tests/CMakeLists.txt;18;add_test;/home/ebraheem/CLionProjects/chess-ex1-ibrahimea/Chess/tests/CMakeLists.txt;0;")
